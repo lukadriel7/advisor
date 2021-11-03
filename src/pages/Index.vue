dabb764b284996fae55e3eb9076514f3799b123e
Yozan Bi Adriel junior <lukadriel@gmail.com>
2021-11-03 10:44:44 +0000

    Add fetch and display of advices

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,12 +8,11 @@
         <q-card-section>
           <q-input
             type="number"
-            debounce="500"
             :label="`Enter a number between ${adviceMinEntries} and ${adviceMaxEntries} (inclusive)`"
             v-model.number="number"
             lazy-rules
             :rules="[
-            (val: number) => val && (val >= 5 && val <= 20) || ''
+            (val: number) => val && (val >= adviceMinEntries && val <= adviceMaxEntries) || ''
           ]"
           />
         </q-card-section>
@@ -28,12 +27,23 @@
         </q-card-actions>
       </q-form>
     </q-card>
+    <div class="q-pa-md">
+      <q-table
+        title="English Advices"
+        :rows="advices"
+        :columns="englishColumns"
+        row-key="name"
+        :loading="advicesLoading"
+      />
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import useUniqueRamdomArray from 'src/composables/useUniqueRandomArray';
+import useAdviceRepositories from 'src/composables/useAdviceRepositories';
+import { englishColumns } from 'components/tableColumns';
 
 const adviceMinEntries = process.env.ADVICE_MIN_ENTRIES;
 const adviceMaxEntries = process.env.ADVICE_MAX_ENTRIES;
@@ -44,13 +54,16 @@ export default defineComponent({
     const number = ref();
 
     const { getUniqueRandomArray, randomArray } = useUniqueRamdomArray(number);
-
+    const { advices, isLoading: advicesLoading } =
+      useAdviceRepositories(randomArray);
     return {
       number,
       adviceMinEntries,
       adviceMaxEntries,
       getUniqueRandomArray,
-      randomArray,
+      englishColumns,
+      advices,
+      advicesLoading,
     };
   },
 });
